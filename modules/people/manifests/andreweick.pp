50e30e1a8b9e2bdc146f23e82d248bd664aaec61
Andrew Eick <maeick@missionfocus.com>
2014-01-18 19:11:42 -0500

    try to pull down dotfiles

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -3,4 +3,17 @@ class people::andreweick {
 	include iterm2::colors::solarized_light
 
 #	include pathfinder
+
+	$dotfiles = "${projects}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'andreweick/dotfiles',
+    require => File[$projects],
+    notify  => Exec['andreweick-make-dotfiles'],
+  }
+
+  # exec { 'andreweick-make-dotfiles':
+  #   command     => "cd ${dotfiles} && make",
+  #   refreshonly => true,
+  # }
 }
