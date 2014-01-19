53805c7fa75f99b21874d816d6f23d9711f380d4
Andrew Eick <maeick@missionfocus.com>
2014-01-19 15:30:22 -0500

    added iterm2 configuration

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -1,19 +1,24 @@
 class people::andreweick {
 	include iterm2::stable
-#	include iterm2::colors::solarized_light
+	include iterm2::colors::solarized_light
 
 	include zsh
 	include dropbox
 	include chrome
   include pathfinder
-	include sublime_text_3
 	include kaleidoscope 
 	include gitx
 	include dropbox
 	include alfred
 	include imagemagick
 
-  $my_home  = "/Users/${::luser}"
+	include sublime_text_3
+	include sublime_text_3::package_control
+	sublime_text_3::package { 'Theme - Soda':
+  	source => 'buymeasoda/soda-theme/'
+	}
+
+	$my_home  = "/Users/${::luser}"
   $projects = "${my_home}/code"
 
   file { $projects:
