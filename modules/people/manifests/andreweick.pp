e091d9f598f0a9c46b2c67c4b4fc53140e419739
Andrew Eick <maeick@missionfocus.com>
2014-01-20 08:57:55 -0500

    updating depencies to point at new versions

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -1,6 +1,6 @@
 class people::andreweick {
 	include iterm2::stable
-#	include iterm2::colors::solarized_light
+	include iterm2::colors::solarized_light
 
 	include zsh
 	include dropbox
@@ -13,10 +13,10 @@ class people::andreweick {
 	include imagemagick
 
 	include sublime_text_3
-	# include sublime_text_3::package_control
-	# sublime_text_3::package { 'Theme - Soda':
-	# 	source => 'buymeasoda/soda-theme/'
-	# }
+	include sublime_text_3::package_control
+	sublime_text_3::package { 'Theme - Soda':
+		source => 'buymeasoda/soda-theme/'
+	}
 
 	$my_home  = "/Users/${::luser}"
   $projects = "${my_home}/code"
