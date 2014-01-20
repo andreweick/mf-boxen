1a6aad355e56af9a6b1afa875d862dc1a1497a88
Andrew Eick <maeick@missionfocus.com>
2014-01-20 13:17:25 -0500

    just cannot get those sub items to work...

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -1,31 +1,31 @@
 class people::andreweick {
-	include iterm2::stable
-#	include iterm2::colors::solarized_light
+  include iterm2::stable
+# include iterm2::colors::solarized_light
 
-	include zsh
-	include dropbox
-	include chrome
+  include zsh
+  include dropbox
+  include chrome
   include pathfinder
-#	include kaleidoscope 
-	include gitx
-	include dropbox
-	include alfred
-	include imagemagick
+# include kaleidoscope 
+  include gitx
+  include dropbox
+  include alfred
+  include imagemagick
 
-	include sublime_text_3
-	include sublime_text_3::package_control
-	sublime_text_3::package { 'Theme - Soda':
-		source => 'buymeasoda/soda-theme/'
-	}
+  include sublime_text_3
+  include sublime_text_3::package_control
+  # sublime_text_3::package { 'Theme - Soda':
+  #   source => 'buymeasoda/soda-theme/'
+  # }
 
-	$my_home  = "/Users/${::luser}"
+  $my_home  = "/Users/${::luser}"
   $projects = "${my_home}/code"
 
   file { $projects:
     ensure => directory,
   }
 
-	$dotfiles = "${projects}/dotfiles"
+  $dotfiles = "${projects}/dotfiles"
 
   repository { $dotfiles:
     source  => 'andreweick/dotfiles',
