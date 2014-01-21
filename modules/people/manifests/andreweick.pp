cfe24e92dfe3a0475275533671171a1b11728fb4
Andrew Eick <maeick@missionfocus.com>
2014-01-21 17:35:33 -0500

    added Font

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -11,12 +11,19 @@ class people::andreweick {
   include dropbox
   include alfred
   include imagemagick
+  include font::source-code-pro
 
   include sublime_text_3
   include sublime_text_3::package_control
+  sublime_text_3::package { 'GitGutter':
+    source => 'jisaacks/GitGutter'
+  }
+
   # sublime_text_3::package { 'Theme - Soda':
   #   source => 'buymeasoda/soda-theme/'
   # }
+
+
 
   $my_home  = "/Users/${::luser}"
   $projects = "${my_home}/code"
