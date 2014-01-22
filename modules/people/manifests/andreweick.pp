6bd900c5d2d0399f49b59fe59d440ff532638986
Andrew Eick <maeick@missionfocus.com>
2014-01-22 15:25:32 -0500

    merged

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -25,28 +25,8 @@ class people::andreweick {
     source => 'jisaacks/GitGutter'
   }
 
-  # sublime_text_3::package { 'Theme - Soda':
-  #   source => 'buymeasoda/soda-theme/'
-  # }
-
-
   # Install my dotfiles
   $my_home  = "/Users/${::luser}"
-=======
-	include zsh
-	include dropbox
-	include chrome
-	include gitx
-	include imagemagick
-
-	include sublime_text_3
-	include sublime_text_3::package_control
-	sublime_text_3::package { 'Theme - Soda':
-		source => 'buymeasoda/soda-theme/'
-	}
-
-	$my_home  = "/Users/${::luser}"
->>>>>>> 6081b2e82c203d2de1e3c51b1cf80b04b42aba40
   $projects = "${my_home}/code"
 
   file { $projects:
