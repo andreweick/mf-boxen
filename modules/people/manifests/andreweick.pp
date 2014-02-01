52576a7ba45e7d4f36c4d89f6474bfb9d7e1173c
Andrew Eick <maeick@missionfocus.com>
2014-02-01 13:53:34 -0500

    changed username back to variable

--- modules/people/manifests/andreweick.pp
+++ modules/people/manifests/andreweick.pp
@@ -91,7 +91,7 @@ class people::andreweick {
 
   # Changes the default shell to the zsh version we get from Homebrew
   # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { ${::luser}:
+  osx_chsh { $my_username:
     shell   => '/opt/boxen/homebrew/bin/zsh',
     require => Package['zsh']
   }
